17482e45424fff924e9f93870ef767714ecaaa4e
Florian Schubert <florian.schubert.sg@gmail.com>
2024-07-23 18:21:17 +0200

    Added Persist Storage Module Squashed commit of the following:
    commit 1c95fb4e062575671c0519bdcc6011ea59925259
    Author: Florian Schubert <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 18:09:25 2024 +0200
    
        Now backups only backup
    
    commit 8a6cb1545dda5f4e1f55663b8f7c07fac56f07e7
    Author: Florian Schubert <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 17:41:31 2024 +0200
    
        System is now a subvolume in config
    
    commit 970c49a5e4ea9b377f5d63c710fa3f86c6336fa2
    Author: Florian Schubert <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 17:39:21 2024 +0200
    
        System is now a subvolume in config
    
    commit 92f6e67b946e671c72323d789dd9aff4d4652756
    Author: Florian Schubert <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 17:38:16 2024 +0200
    
        System is now a subvolume in config
    
    commit 600090655d0c83ec24cd03ad4e8d6c286e8d6ff3
    Author: Florian Schubert <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 17:31:55 2024 +0200
    
        Fixed btrbk
    
    commit 266b3076487308deac6a9c4cd2c052d3b451b097
    Author: florian <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 17:01:39 2024 +0200
    
        Fixed btrbk settings
    
    commit 527d290a391e1af1fee7d5a56ebdb737a34faa10
    Author: florian <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 16:59:13 2024 +0200
    
        Fixed btrbk settings
    
    commit e7dc65765f9a5ed78f1fad3aa79c96d953101933
    Author: florian <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 16:49:29 2024 +0200
    
        Fixed btrbk settings
    
    commit c7cacff353e8d5c25e3d913fa5adefff5661afbd
    Author: florian <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 16:47:31 2024 +0200
    
        Fixed btrbk settings
    
    commit 94f0816512a930524bd985af01efc4b832422847
    Author: florian <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 16:04:42 2024 +0200
    
        Included Impermanence in flake
    
    commit 418493f77c46033c6719c95dc353950d02becb44
    Author: Florian Schubert <30634234+JamesofScout@users.noreply.github.com>
    Date:   Tue Jul 23 14:50:35 2024 +0200
    
        Update persist.nix
    
    commit c8c9de7bb1e1a9e9495c042565e46ba6de57c08e
    Author: Florian Schubert <30634234+JamesofScout@users.noreply.github.com>
    Date:   Tue Jul 23 14:10:05 2024 +0200
    
        Update persist.nix
    
    commit a75bae1aa59a6a5925ef1f1756b45ad6e3928c73
    Author: Florian Schubert <30634234+JamesofScout@users.noreply.github.com>
    Date:   Tue Jul 23 13:22:42 2024 +0200
    
        Update flake.nix
    
    commit f3cfe792109702f5fb98eb09f6dd259a8d4676fb
    Author: Florian Schubert <30634234+JamesofScout@users.noreply.github.com>
    Date:   Tue Jul 23 13:22:27 2024 +0200
    
        Update flake.nix
    
    commit caa035e22579c4a2d8aa50be3181e0d5343b6896
    Author: florian <florian.schubert.sg@gmail.com>
    Date:   Tue Jul 23 12:38:17 2024 +0200
    
        Began Configuration for Automatic Snapshots

---
+++ storage/persist.nix
@@ -0,0 +1,168 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let opts = config.nix-tun.storage.persist; in {
+
+  options.nix-tun.storage.persist = {
+    enable = lib.mkEnableOption ''
+      A wrapper arround impermanence and btrbk. Expects a btrfs filesystem with the following layout:
+	- /root <- The actual root mounted at / 
+	- /nix <- The root for all things nix. Mounted at /nix
+	- /persist <- The root of all other persistent storage, mounted at /persist
+
+      *Note*: For systems that use more than one (logical) drive, simply mount more  
+    '';
+    persistentFullHome = lib.mkEnableOption "Enable if simply all of Home should be Persistent";
+    path = lib.mkOption {
+      type = lib.types.str;
+      default = "/persist";
+      description = ''
+      The root directory for all of non generated persistent storage, except /nix and /boot. 
+      '';
+    };
+    subvolumes = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.submodule ({...}: {
+        options = {
+          owner = lib.mkOption {
+            type = lib.types.str;
+            default = "root";
+            description = ''
+              The owner of the subvolume
+            '';
+          };
+          group = lib.mkOption {
+            type = lib.types.str;
+            default = "root";
+            description = ''
+              The group of the subvolume
+            '';
+          };
+          mode = lib.mkOption {
+            type = lib.types.str;
+            default = "0755";
+            description = "The mode of the subvolume, default is 0755";
+          };
+          backup = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Whether this subvolume should be backuped, default is true";
+          };
+	  bindMountDirectories = lib.mkOption {
+	    type = lib.types.bool;
+	    default = false;
+	    description = ''
+	    Should all directoris inside this subvolume be bind-mounted to their respective paths in / (according to their name). 
+	    '';
+	  };
+	  directories = lib.mkOption {
+	    type = lib.types.attrsOf( lib.types.submodule ({...} : {
+	      options = {
+	      owner = lib.mkOption {
+	        type = lib.types.str; default = "root"; };
+	      group = lib.mkOption {
+	        type = lib.types.str;
+		default = "root";
+	      };
+	      mode = lib.mkOption {
+	        type = lib.types.str;
+	        default = "0755";
+	      };
+	      };
+	    }));
+	    default = {};
+	    description = ''
+	      Directories that should be created per default inside the subvolume
+	    '';
+	  };
+        };
+      }));
+      default = {
+      };
+      description = ''
+        Subvolumes that should be persistent.
+      '';
+    };
+  };
+
+  config = lib.mkIf opts.enable {
+    nix-tun.storage.persist.subvolumes = {
+        system.directories = {
+	  "/var/log" = {};
+	  "/var/lib/nixos" = {}; # For Correct User Mapping
+	  "/var/lib/systemd/coredump" = {};
+	};
+	# Storage for the SSH Host Keys - Are not part of the backup
+	ssh-keys = {
+	  backup = false;
+	};
+
+    };
+
+    systemd.tmpfiles.rules = builtins.concatLists (lib.attrsets.mapAttrsToList (
+      name: value: 
+      [
+	"v ${opts.path}/${name} ${value.mode} ${value.owner} ${value.group} -"
+	(lib.mkIf value.backup "d ${opts.path}/${name}/.snapshots ${value.mode} ${value.owner} ${value.group} -")
+      ] 
+      ++ lib.attrsets.mapAttrsToList (n: v:
+        "d ${opts.path}/${name}/${n} ${v.mode} ${v.owner} ${v.group} -"
+      ) value.directories
+    )
+    opts.subvolumes);
+
+    environment.persistence = lib.mapAttrs' (name: value: 
+    {
+      name = "${opts.path}/${name}";
+      value = {
+	hideMounts = true;
+	directories = lib.mapAttrsToList (name: value:
+	  {
+	    directory = name;
+	    user = value.owner;
+	    group = value.group;
+	    mode = value.mode;
+	  }
+	  #(lib.mkIf config.nix-tun.storage.persist.persistentFullHome "/home")
+	  #(lib.mkIf config.networking.networkmanager.enable "/etc/NetworkManager/system-connections")
+	  #(lib.mkIf config.services.printing.enable "/var/lib/cups")
+	) value.directories;
+	files = [
+	];
+      };
+    }) (lib.attrsets.filterAttrs (name: value: value.bindMountDirectories) opts.subvolumes);
+
+    services.btrbk.instances.btrbk.settings  = {
+      snapshot_preserve = "7d";
+      snapshot_preserve_min = "7d";
+      timestamp_format = "long-iso";
+
+      volume = lib.attrsets.mapAttrs' (name: value: {
+        name = "${opts.path}/${name}";
+	value = {
+	  subvolume = "/";
+	  snapshot_dir = ".snapshots";
+	};
+      }) (lib.attrsets.filterAttrs (name: value: value.backup) opts.subvolumes);
+    };
+
+    services.openssh.hostKeys = [
+      {
+        bits = 4096;
+        openSSHFormat = true;
+        path = "${opts.path}/ssh-keys/ssh_host_rsa_key";
+        rounds = 100;
+        type = "rsa";
+      }
+      {
+        comment = "key comment";
+        path = "${opts.path}/ssh-keys/ssh_host_ed25519_key";
+        rounds = 100;
+        type = "ed25519";
+      }
+    ];
+  };
+}
+

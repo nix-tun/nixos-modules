e07c95348b5aad2011c2b7c80b4bce26efd35362
florian <florian.schubert.sg@gmail.com>
2024-08-24 19:46:07 +0200

    temp: removed need for adminpass

--- services/containers/nextcloud.nix
+++ services/containers/nextcloud.nix
@@ -31,9 +31,9 @@
     opts = config.teenix.services.nextcloud;
   in
     lib.mkIf opts.enable {
-      sops.secrets.nextcloud_pass = {
-        mode = "444";
-      };
+      #sops.secrets.nextcloud_pass = {
+      #  mode = "444";
+      #};
 
       nix-tun.utils.containers.nextcloud.volumes = {
         "/var/lib/mysql" = {
@@ -80,7 +80,7 @@
 
             settings.trusted_domains = ["192.168.100.11" opts.hostname];
             config = {
-              adminpassFile = config.sops.secrets.nextcloud_pass.path;
+              #adminpassFile = config.sops.secrets.nextcloud_pass.path;
               dbtype = "mysql";
             };
 

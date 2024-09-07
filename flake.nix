78694af3de5a479d91c88bf9fef035fe9980cd83
Florian Schubert <florian.schubert.sg@gmail.com>
2024-09-07 23:21:47 +0200

    added flake.nix

--- flake.nix
+++ flake.nix
@@ -7,33 +7,36 @@
     authentik-nix.url = "github:nix-community/authentik-nix";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: let
-    systems = [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
-
-    lib = nixpkgs.lib;
-
-    forAllSystems = lib.genAttrs systems;
-  in {
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-
-    nixosModules.nix-tun = {pkgs, ...}: {
-      imports = [
-        ./yubikey-gpg.nix
-        ./storage/persist.nix
-        ./storage/backup-server.nix
-        ./services/containers/nextcloud.nix
-        ./services/containers/authentik.nix
-        ./utils/container.nix
-        #./services/matrix.nix
-
-        inputs.impermanence.nixosModules.impermanence
-        ./services/traefik.nix
+  outputs = { nixpkgs, ... } @ inputs:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
+
+      lib = nixpkgs.lib;
+
+      forAllSystems = lib.genAttrs systems;
+    in
+    {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+      nixosModules.nix-tun = { pkgs, ... }: {
+        imports = [
+          ./yubikey-gpg.nix
+          ./storage/persist.nix
+          ./storage/backup-server.nix
+          ./services/containers/nextcloud.nix
+          ./services/containers/authentik.nix
+          ./services/containers/onlyoffice.nix
+          ./utils/container.nix
+          #./services/matrix.nix
+
+          inputs.impermanence.nixosModules.impermanence
+          ./services/traefik.nix
+        ];
+      };
     };
-  };
 }
---
+++ services/containers/onlyoffice.nix
@@ -0,0 +1,77 @@
+{ lib
+, config
+, inputs
+, pkgs
+, ...
+}: {
+  options.nix-tun.services.containers.onlyoffice = {
+    enable = lib.mkEnableOption "setup authentik";
+    hostname = lib.mkOption {
+      type = lib.types.str;
+    };
+    jwtSecretFile = lib.mkOption {
+      type = lib.types.path;
+      description = "path to the sops secret file for jwt secret website Server";
+    };
+  };
+
+  config =
+    let
+      opts = config.nix-tun.services.containers.authentik;
+    in
+    lib.mkIf opts.enable {
+      sops.secrets.onlyoffice_jwt = {
+        sopsFile = opts.envFile;
+        format = "binary";
+        mode = "444";
+      };
+
+      nix-tun.services.traefik.services."authentik" = {
+        router.rule = "Host(`${opts.hostname}`)";
+        servers = [ "http://${config.containers.onlyoffice.config.networking.hostName}:8000" ];
+      };
+
+      containers.onlyoffice = {
+        ephemeral = true;
+        autoStart = true;
+        privateNetwork = true;
+        hostAddress = "192.168.131.10";
+        localAddress = "192.168.131.11";
+
+        bindMounts = {
+          "secret" = {
+            hostPath = config.sops.secrets.onlyoffice_jwt.path;
+            mountPoint = config.sops.secrets.onlyoffice_jwt.path;
+          };
+        };
+
+        config = { ... }: {
+          imports = [
+            inputs.authentik-nix.nixosModules.default
+          ];
+
+          networking.hostName = "onlyoffice";
+
+          services.onlyoffice = {
+            enable = true;
+            hostname = opts.hostname;
+            jwtSecretFile = config.sops.secrets.onlyoffice_jwt.path;
+
+            networking = {
+              firewall = {
+                enable = true;
+                allowedTCPPorts = [ 8000 ];
+              };
+              # Use systemd-resolved inside the container
+              # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+              useHostResolvConf = lib.mkForce false;
+            };
+
+            services.resolved.enable = true;
+
+            system.stateVersion = "23.11";
+          };
+        };
+      };
+    };
+}

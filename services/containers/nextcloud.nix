225c9735792b8ef87b562509895df426144dedde
florian <florian.schubert.sg@gmail.com>
2025-03-07 13:09:05 +0100

    updated nextcloud

--- services/containers/nextcloud.nix
+++ services/containers/nextcloud.nix
@@ -80,7 +80,7 @@
           ];
           services.nextcloud = {
             enable = true;
-            package = pkgs.nextcloud30;
+            package = pkgs.nextcloud31;
             https = true;
             hostName = opts.hostname;
             phpExtraExtensions = all: [ all.pdlib all.smbclient ];

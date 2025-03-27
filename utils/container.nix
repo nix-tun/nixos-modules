d597d76cda18a6cba013b47af0f42908b9cdb37f
florian <florian.schubert.sg@gmail.com>
2025-03-27 14:41:49 +0100

    feat(utils-container): added config directly into container

--- utils/container.nix
+++ utils/container.nix
@@ -1,79 +1,97 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
+{ lib
+, config
+, pkgs
+, ...
 }: {
   # Utils to make the configuration of NixOs Containers more streamlined, in Context of the entire flake.
   options = {
     nix-tun.utils.containers = lib.mkOption {
-      type = lib.types.attrsOf (lib.types.submodule ({...}: {
-        options = {
-          volumes = lib.mkOption {
-            description = ''
-                     Directories to autmatically create in persistent storage, and bind mount inside the container.
-              Directories will be created in /persist/containers/<container-name>/<directory>.
-              Where /persist/containers/<container-name> is a btrfs Subvolume that will be snapshoted daily.
-            '';
-            type = lib.types.attrsOf (lib.types.submodule ({...}: {
-              options = {
-                owner = lib.mkOption {
-                  type = lib.types.str;
-                  description = ''
-                    The username of the owner of the Directory (of an user declared inside the container)
-                  '';
-                  default = "root";
+      type = lib.types.attrsOf
+        (lib.types.submodule
+          ({ config, options, name, ... }: {
+            options = {
+              config = lib.mkOption
+                {
+                  type = lib.mkOptionType {
+                    name = "Toplevel NixOs config";
+                    merge = loc: defs: (import "${config.nixpkgs}/nixos/lib/eval-config.nix" {
+                      modules = (map (x: x.value) defs);
+                      prefix = [ "nix-tun" "containers" name ];
+                      inherit (config) specialArgs;
+                    }).config;
+                  };
+
                 };
-                group = lib.mkOption {
-                  type = lib.types.str;
-                  description = ''
-                    The name of the group of the Directory. (of a group inside the container)
-                  '';
-                  default = "root";
-                };
-                mode = lib.mkOption {
-                  type = lib.types.str;
-                  description = ''
-                    The mode of the directory
-                  '';
-                  default = "0700";
-                };
+              volumes = lib.mkOption {
+                description = ''
+                  Directories to autmatically create in persistent storage, and bind mount inside the container.
+                  Directories will be created in /persist/containers/<container-name>/<directory>.
+                  Where /persist/containers/<container-name> is a btrfs Subvolume that will be snapshoted daily.
+                '';
+                type = lib.types.attrsOf (lib.types.submodule ({ ... }: {
+                  options = {
+                    owner = lib.mkOption {
+                      type = lib.types.str;
+                      description = ''
+                        The username of the owner of the Directory (of an user declared inside the container)
+                      '';
+                      default = "root";
+                    };
+                    group = lib.mkOption {
+                      type = lib.types.str;
+                      description = ''
+                        The name of the group of the Directory. (of a group inside the container)
+                      '';
+                      default = "root";
+                    };
+                    mode = lib.mkOption {
+                      type = lib.types.str;
+                      description = ''
+                        The mode of the directory
+                      '';
+                      default = "0700";
+                    };
+                  };
+                }));
               };
-            }));
-          };
-        };
-      }));
-      default = {};
+            };
+          }));
+      default = { };
       description = ''
         Utils to make the configuration of NixOs Containers more streamlined, in Context of the entire flake.
       '';
     };
   };
 
-  config = {
-    nix-tun.storage.persist.subvolumes =
-      lib.attrsets.mapAttrs' (name: value: {
-        name = "containers/${name}";
-        value.directories =
-          lib.attrsets.mapAttrs (_: value: {
-            owner = builtins.toString config.containers."${name}".config.users.users.${value.owner}.uid;
-            group = builtins.toString config.containers."${name}".config.users.groups.${value.group}.gid;
-            mode = value.mode;
+  config =
+    {
+      nix-tun.storage.persist.subvolumes =
+        lib.attrsets.mapAttrs'
+          (name: value: {
+            name = "containers/${name}";
+            value.directories =
+              lib.attrsets.mapAttrs
+                (_: value: {
+                  owner = builtins.toString config.containers."${name}".config.users.users.${value.owner}.uid;
+                  group = builtins.toString config.containers."${name}".config.users.groups.${value.group}.gid;
+                  mode = value.mode;
+                })
+                value.volumes;
           })
-          value.volumes;
-      })
-      config.nix-tun.utils.containers;
+          config.nix-tun.utils.containers;
 
-    containers =
-      lib.attrsets.mapAttrs (name: value: {
-        bindMounts =
-          lib.attrsets.mapAttrs (n: value: {
-            hostPath = "${config.nix-tun.storage.persist.path}/containers/${name}/${n}";
-            mountPoint = n;
-            isReadOnly = false;
+      containers =
+        lib.attrsets.mapAttrs
+          (name: value: {
+            bindMounts =
+              lib.attrsets.mapAttrs
+                (n: value: {
+                  hostPath = "${config.nix-tun.storage.persist.path}/containers/${name}/${n}";
+                  mountPoint = n;
+                  isReadOnly = false;
+                })
+                value.volumes;
           })
-          value.volumes;
-      })
-      config.nix-tun.utils.containers;
-  };
+          config.nix-tun.utils.containers;
+    };
 }

6db49196972dbad2b4394baef136eee95b9edcdd
Florian Schubert <florian.schubert.sg@gmail.com>
2025-12-18 13:26:30 +0100

    disabled syntehtic monitoring plugin for grafana

--- services/grafana.nix
+++ services/grafana.nix
@@ -201,13 +201,13 @@
 
             services.grafana =
               let
-                provisionPath = pkgs.runCommand "grafana-provisioning" lib.strings.concatLines
+                provisionPath = pkgs.runCommand "grafana-provisioning" (lib.strings.concatLines
                   (lib.attrsets.mapAttrsToList
                     (category: settings: ''
                       mkdir -p $out/${category}
                       ${(pkgs.formats.yaml {}).generate "$out/${category}/${category}.yaml" settings}
                     '')
-                    config.nix-tun.services.grafana.provision);
+                    config.nix-tun.services.grafana.provision));
               in
               {
                 enable = true;

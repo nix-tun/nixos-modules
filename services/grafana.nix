a95b918e992ce6d7ff8597ae532d08a8007089ce
Florian Schubert <florian.schubert.sg@gmail.com>
2026-07-06 17:59:15 +0200

    grafana: added oauth

--- services/grafana.nix
+++ services/grafana.nix
@@ -72,6 +72,7 @@
           auth_url = "https://${config.nix-tun.services.authelia.domain}/api/oidc/authorization";
           api_url = "https://${config.nix-tun.services.authelia.domain}/api/oidc/userinfo";
           token_url = "https://${config.nix-tun.services.authelia.domain}/api/oidc/token";
+          role_attribute_path = "contains(groups[*], 'admin') && 'Admin' || contains(groups[*], 'editor') && 'Editor' || 'Viewer'";
           auto_login = true;
         };
 
